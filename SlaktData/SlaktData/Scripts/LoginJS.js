f3e55da58880dcc3e4ecdf7d0ac0d168437d5f22
Meelad <meelad.sd@hotmail.com>
2016-05-03 13:17:01 +0200

    login view

---
+++ SlaktData/SlaktData/Scripts/LoginJS.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#user-name').focus(
+            function () {
+                $('.login-field-user').css({
+                    "border-left": "solid 3px #337ab7",
+                    "background-color": "#fbfbfb"
+                    })
+            }
+        ).blur(
+            function () {
+                $('.login-field-user').css({
+                    "border-left": "solid 3px rgba(0, 0, 0, 0.00)",
+                    "background-color": "#fff"
+                });
+            }
+        );
+    $('#user-password').focus(
+            function () {
+                $('.login-field-pass').css({
+                    "border-left": "solid 3px #337ab7",
+                    "background-color": "#fbfbfb"
+                })
+            }
+        ).blur(
+            function () {
+                $('.login-field-pass').css({
+                    "border-left": "solid 3px rgba(0, 0, 0, 0.00)",
+                    "background-color": "#fff"
+                });
+            }
+        );
+});
